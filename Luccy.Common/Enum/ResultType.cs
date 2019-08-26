02b07c6f6b07630a28b7a14bb870fb6aac2e59e2
xuzu <1270091284@qq.com>
2019-08-27 00:24:39 +0800

    2019/8/26

---
+++ Luccy.Common/Enum/ResultType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luccy.Common.Enum
+{
+    /// <summary>
+    /// 表示 ajax 操作结果类型的枚举
+    /// </summary>
+    public enum ResultType
+    {
+        /// <summary>
+        /// 消息结果类型
+        /// </summary>
+        info,
+        /// <summary>
+        /// 成功结果类型
+        /// </summary>
+        success,
+        /// <summary>
+        /// 警告结果类型
+        /// </summary>
+        warning,
+        /// <summary>
+        /// 异常结果类型
+        /// </summary>
+        error
+    }
+}
---
+++ Luccy.Common/Utils/MD5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luccy.Common.Utils
+{
+    /// <summary>
+    /// MD5加密
+    /// </summary>
+    public class Md5
+    {
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="str">加密字符</param>
+        /// <param name="code">加密位数16/32</param>
+        /// <returns></returns>
+        public static string GetMD5(string myString)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = System.Text.Encoding.Unicode.GetBytes(myString);
+            byte[] targetData = md5.ComputeHash(fromData);
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in targetData)
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            return ret.ToString();
+        }
+    }
+}
